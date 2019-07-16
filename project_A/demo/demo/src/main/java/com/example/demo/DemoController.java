491478443b7344006b7b35ab86768dc25d178661
Rick <rickchongrockching@gmail.com>
2019-07-16 19:17:44 +0800

    1.5 endpoints done

--- project_A/demo/demo/src/main/java/com/example/demo/DemoController.java
+++ project_A/demo/demo/src/main/java/com/example/demo/DemoController.java
@@ -1,48 +1,78 @@
 package com.example.demo;
 
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.boot.json.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.*;
 
 @RestController
-public class DemoController {
-	
-	@GetMapping("/account")
-	public String GetAccountByID(short id) throws IOException
-	{
-		String theString = "https://jsonplaceholder.typicode.com/todos/" + Integer.toString(id);
-        RestTemplate restTemplate = new RestTemplate();
-        theString = restTemplate.getForObject(theString, String.class);
-
+public class DemoController extends RestTemplate
+{
+    
+    /*@GetMapping("/account")
+    public ResponseEntity<String> GetAccountByID(@RequestParam short id) throws IOException
+    {
+        ResponseEntity<String> entity = getForEntity("https://jsonplaceholder.typicode.com/todos/" + Integer.toString(id), String.class);
+        String body = entity.getBody();
+        //System.out.println(body + "i was edited lmao");
         JsonParser springParser = JsonParserFactory.getJsonParser();
-
-        //List<Object> list = springParser.parseList(theString);
-        Map<String, Object> result = springParser.parseMap(theString);
-        return theString;
-        /*theString = "";
-        for (Map.Entry<String, Object> entry : result.entrySet())
-		{
-			theString += entry.getKey() + " = " + entry.getValue() + "\n";
-		}*/
-
-        /*for(Object o : list)
-        {
-        	if(o instanceof Map)
-        	{
-        		Map<String,Object> map = (Map<String,Object>) o;
-        		System.out.println("Items found: " + map.size());
-
-        		for (Map.Entry<String, Object> entry : map.entrySet())
-        		{
-        			theString += entry.getKey() + " = " + entry.getValue() + "\n";
-        		}
-
-        	}
-        }*/
-	}
-
+        Map<String, Object> result = springParser.parseMap(body);
+        return entity;
+    }
+    
+    @PostMapping("/account")
+    public ResponseEntity<String> PostAccount(@RequestParam short id) throws IOException
+    {
+        ResponseEntity<String> entity = getForEntity("https://jsonplaceholder.typicode.com/todos/" + Integer.toString(id), String.class);
+        String body = entity.getBody();
+        //System.out.println(body + "i was edited lmao");
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String, Object> result = springParser.parseMap(body);
+        return entity;
+    }
+    
+    @UpdateMapping("/account")
+    public ResponseEntity<String> UpdateAccount(@RequestParam short id) throws IOException
+    {
+        ResponseEntity<String> entity = getForEntity("https://jsonplaceholder.typicode.com/todos/" + Integer.toString(id), String.class);
+        String body = entity.getBody();
+        //System.out.println(body + "i was edited lmao");
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String, Object> result = springParser.parseMap(body);
+        return entity;
+    }
+    
+    @DeleteMapping("/account")
+    public ResponseEntity<String> DeleteAccount(@RequestParam short id) throws IOException
+    {
+        ResponseEntity<String> entity = getForEntity("https://jsonplaceholder.typicode.com/todos/" + Integer.toString(id), String.class);
+        String body = entity.getBody();
+        //System.out.println(body + "i was edited lmao");
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String, Object> result = springParser.parseMap(body);
+        return entity;
+    }*/
+	
+	@GetMapping("/alluser")
+    public ResponseEntity<String> GETalluser() throws IOException
+    {
+        return getForEntity("https://sheetdb.io/api/v1/x7q7rbu7cdn5w?sheet=user-account", String.class);
+        //String body = entity.getBody();
+        //System.out.println(body + "i was edited lmao");
+        //ObjectMapper objectMapper = new ObjectMapper();
+    }
+	
+	@PostMapping("/createuser")
+    public ResponseEntity<String> POSTcreateuser(@RequestParam DemoJSONClass account) throws IOException
+    {
+        return getForEntity("https://sheetdb.io/api/v1/x7q7rbu7cdn5w?sheet=user-account", String.class);
+        //String body = entity.getBody();
+        //System.out.println(body + "i was edited lmao");
+        //ObjectMapper objectMapper = new ObjectMapper();
+    }
 }
